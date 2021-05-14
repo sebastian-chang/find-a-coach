fbb59a0b68cbde964a9525b5f44b9b01c7a33e17
Sebastian <sebastian.chang23@gmail.com>
2021-05-13 20:56:14 -0500

    Created not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found. Please check out all our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
